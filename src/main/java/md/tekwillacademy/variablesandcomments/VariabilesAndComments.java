7add1cf2bbcf35747888c388dc265037b431d042
gabriela.babara <gabriela.babara@gmail.com>
2023-09-15 13:39:46 +0300

    Added the Variables and Comments Task

---
+++ src/main/java/md/tekwillacademy/variablesandcomments/VariabilesAndComments.java
@@ -0,0 +1,38 @@
+package md.tekwillacademy.variablesandcomments;
+public class VariabilesAndComments {
+        public static void main (String [] args){
+//Declare the primitive dara type variables
+            boolean falseBooleanVariable;
+            boolean trueBooleanVariable;
+            char firstCharFromAscii;
+            char lastCharFromAscii;
+            byte lowerLimitOfByteDataType;
+            byte uppperLimitOfByteDataType;
+            short lowerLimitOfShortDataType;
+            short uppperLimitOfShortDataType;
+            int lowerLimitOfIntDataType;
+            int uppperLimitOfIntDataType;
+            long lowerLimitOfLongDataType;
+            long uppperLimitOfLongDataType;
+            float lowerLimitOfFloatDataType;
+            float uppperLimitOfFloatDataType;
+            double lowerLimitOfDoubleDataType;
+            double uppperLimitOfDoubleDataType;
+
+//Initialization of the above variable
+            falseBooleanVariable = false;
+            trueBooleanVariable = true;
+            lowerLimitOfByteDataType = -128;
+            uppperLimitOfByteDataType = 127;
+            lowerLimitOfShortDataType = -32768;
+            uppperLimitOfShortDataType = 32767;
+            lowerLimitOfIntDataType = -2147483648;
+            uppperLimitOfIntDataType = 2147483647;
+            lowerLimitOfLongDataType = -9223372036854775808L;
+            uppperLimitOfLongDataType = 9223372036854775807L;
+            lowerLimitOfFloatDataType = -3.4E+38f;
+            uppperLimitOfFloatDataType = 3.4E+38f;
+            lowerLimitOfDoubleDataType = -1.7E+308d;
+            uppperLimitOfDoubleDataType = 1.7E+305;
+        }
+    }
